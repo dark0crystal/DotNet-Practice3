60e19ebac35950d9ba7853754932c6c8f6f79089
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-06 12:34:23 +0400

    dynamic binding form

--- DotNet-Practice3/Pages/Index.cshtml.cs
+++ DotNet-Practice3/Pages/Index.cshtml.cs
@@ -1,18 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DotNet_Practice3.Pages;
+namespace DotNet_Practice3.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+        public string uname = "";
+        public string pass = "";
 
-public class IndexModel : PageModel
-{
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-    }
+        public IndexModel(ILogger<IndexModel> logger){
+            _logger = logger;
+        }
+        public void OnPost (string uname,string pass)
+        {
+            this.uname = uname; this.pass=pass;
+        } 
+    } 
 }
